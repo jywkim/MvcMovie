fca62f114306f44f03bd1b95bee93367c3ceaaa8
jywkim <jkim589@gmail.com>
2018-10-17 00:10:49 -0400

    pass parameter info from URL to controller

--- MvcMovie/Controllers/HelloWorldController.cs
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -15,10 +15,18 @@ namespace MvcMovie.Controllers
 
         // 
         // GET: /HelloWorld/Welcome/ 
-
-        public string Welcome()
+        // Requires using System.Text.Encodings.Web;
+        // http://localhost:xxxx/HelloWorld/Welcome?name=Jon&numtimes=6
+        /*
+        public string Welcome(string name, int numTimes = 1)
         {
-            return "This is the Welcome action method...";
+            return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+        }
+        */
+        // http://localhost:xxx/HelloWorld/Welcome/6?name=Jon
+        public string Welcome(string name, int ID = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
         }
     }
 }
